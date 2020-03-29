a0071d21f777f472e0b09de43336213a0606efbf
Rashid Lasker <rashidlasker@gmail.com>
2020-03-29 17:10:18 -0400

    add winner to game over

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -8,7 +8,7 @@ const PROD_API_HOST = 'https://hoohacks-2020-272521.appspot.com';
 // PRODUCTION defined in webpack config.
 export const API_HOST = PRODUCTION ? PROD_API_HOST : DEV_API_HOST;
 
-
 const DEV_SOCKET_HOST = 'ws://1fb66012.ngrok.io'; // socket server
+// const DEV_SOCKET_HOST = 'ws://127.0.0.1:6789';
 const PROD_SOCKET_HOST = 'wss://socket.poseparty.brian.lol:6789';
 export const SOCKET_HOST = PRODUCTION ? PROD_SOCKET_HOST : DEV_SOCKET_HOST;
